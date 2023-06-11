b9122954ee838456c0809943e36e6db47f477478
Florent Descroix <florentdescroix@posteo.net>
2023-06-11 17:33:20 +0200

    Refacto, optimization and datascructure

--- src/components/LyricsEditor.vue
+++ src/components/LyricsEditor.vue
@@ -17,120 +17,31 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 -->
 
 <template>
-  <div class="toolbar-wrapper">
-    <div :id="`toolbar-${uuid}`"></div>
-  </div>
-  <div class="editor-container">
-    <Editor is="editor" class="editor" v-bind="$attrs" initial-value="modelValue"
-      api-key="obxtg43v00kcomg2zvm4hsgcmpzk37sg7bwcimz3im92ueqp" :init="init" v-model="value" @click="click">
-    </Editor>
-  </div>
+  <Editor is="editor" class="editor" ref="editor" @click="click" :init="init" v-model="value" />
 </template>
 
 <script>
 import Editor from '@tinymce/tinymce-vue'
-import { Voices } from '@/helpers/Constants'
-
-const LyricsElements = {
-  section: ["verse", "chorus"],
-  voice: Object.keys(Voices),
-  repeat: ["x2", "x3", "x4", "x5"],
-}
-
-let uuid = 0
+import { genInit } from '@/helpers/TinyMce'
 
 export default {
   name: 'LyricsEditor',
   components: { Editor },
-  inheritAttrs: false,
   emits: ["update:modelValue"],
   props: {
     modelValue: {
-      type: String,
-      default: "",
+      type: [Array, String],
+      default: () => [],
     }
   },
   data() {
-    uuid++
     return {
-      value: this.modelValue,
-      uuid: uuid,
+      value: typeof this.modelValue == 'string' ? this.modelValue : this.toHTML({
+        div: 'editor',
+        content: this.modelValue
+      }).innerHTML,
       buttons: {},
-      init: {
-        inline: true,
-        menubar: false,
-        statusbar: false,
-        paste_block_drop: false,
-        newline_behavior: 'invert',
-        forced_root_block: "section",
-        forced_root_block_attrs: { class: "verse" },
-        invalid_elements: "*",
-        valid_children: `-section[section],-repeat[section]`,
-        valid_classes: {
-          section: LyricsElements.section.join(" "),
-          voice: LyricsElements.voice.join(" "),
-          repeat: LyricsElements.repeat.join(" ").replaceAll('x', '')
-        },
-        valid_elements: "br,section[!class],repeat[!class],voice[!class]",
-        custom_elements: "section,repeat,~voice",
-        fixed_toolbar_container: `#toolbar-${uuid}`,
-        toolbar: "bass tenor alto soprano | x1 x2 x3 x4 x5 | removeformat",
-        formats: {
-          removeformat: { selector: 'repeat,voice', remove: 'all', deep: true },
-          section: { block: "section", attributes: { class: '%value' } },
-          repeat: { block: "repeat", attributes: { class: '%value' }, merge_siblings: false },
-          voice: { inline: "voice", classes: '%value', block_expand: true, split: false }
-        },
-        setup: (editor) => {
-          this.editor = editor
-
-          for (const name of LyricsElements.voice) {
-            editor.ui.registry.addToggleButton(name, {
-              text: this.$t(`short.${name}`),
-              onAction: () => {
-                editor.formatter.toggle("voice", { value: name })
-              },
-              onSetup: (api) => {
-                this.buttons[name] = api
-                api.setEnabled(this.voices[name]?.note)
-                api.setActive(editor.formatter.match("voice", { value: name }))
-                editor.formatter.formatChanged("voice", (state) => {
-                  api.setActive(state)
-                }, false, { value: name })
-                editor.formatter.formatChanged("voice", (state) => {
-                  api.setActive(state)
-                }, false, { value: name })
-              }
-            })
-          }
-
-          for (const name of LyricsElements.repeat) {
-            const value = name.slice(-1)
-            editor.ui.registry.addToggleButton(name, {
-              text: this.$t(`short.${name}`),
-              onAction: () => {
-                if (editor.selection.isCollapsed() && editor.formatter.match('repeat', { value })) {
-                  editor.formatter.remove('repeat', { value }, editor.selection.getNode())
-                } else if (!editor.selection.isCollapsed()) {
-                  const selectedContent = editor.selection.getContent()
-                  if (!selectedContent.includes("section")) {
-                    const element = document.createElement('repeat')
-                    element.className = value
-                    element.innerHTML = selectedContent
-                    editor.selection.setContent(element.outerHTML)
-                  }
-                }
-              },
-              onSetup: (api) => {
-                api.setActive(editor.formatter.match('repeat', { value }))
-                editor.on('SelectionChange', () => {
-                  api.setActive(editor.selection.isCollapsed() && editor.formatter.match('repeat', { value }))
-                })
-              }
-            })
-          }
-        }
-      }
+      init: genInit('#song .body', this.setup)
     }
   },
   computed: {
@@ -140,23 +51,59 @@ export default {
   },
   watch: {
     value() {
-      this.$emit('update:modelValue', this.value)
+      const div = document.createElement('div')
+      div.innerHTML = this.value
+      this.$emit('update:modelValue', this.toJson(div).content)
     },
     voices: {
       deep: true,
       handler() {
-        for (const name in this.voices) {
-          this.buttons?.[name]?.setEnabled(this.voices[name]?.note)
-        }
+        this.updateVoicesButton()
       }
     }
   },
   methods: {
+    setup(editor, voicesApi) {
+      this.editor = editor
+      this.buttons = voicesApi
+      this.updateVoicesButton()
+    },
+    updateVoicesButton() {
+      for (const name in this.voices) {
+        this.buttons?.[name]?.setEnabled(this.voices[name]?.note)
+      }
+    },
     click(e, editor) {
-      if (editor && e.target?.tagName == 'SECTION') {
-        editor.formatter.toggle("section", { value: 'chorus' })
-      } else if (editor && e.target?.tagName == 'REPEAT') {
-        editor.formatter.remove("repeat", { value: e.target.className }, e.target)
+      // Two events are triggered : native's and editor's
+      if (editor) {
+        const box = e.target.getBoundingClientRect()
+        if (e.clientX < box.left && e.target?.tagName == 'SECTION') {
+          editor.formatter.toggle("section", { value: 'chorus' })
+        } else if (e.clientX < box.left && e.target?.tagName == 'REPEAT') {
+          editor.formatter.remove("repeat", { value: e.target.className }, e.target)
+        }
+      }
+    },
+    toJson(node) {
+      if (node.nodeType == 3) {
+        return node.textContent
+      } else {
+        return {
+          [node.tagName.toLowerCase()]: node.className,
+          content: [...node.childNodes].map(this.toJson).filter(part => part.br != undefined || part?.length || part?.content?.length)
+        }
+      }
+    },
+    toHTML(obj) {
+      if (typeof obj == 'string') {
+        return document.createTextNode(obj)
+      } else {
+        const tag = Object.keys(obj).find(key => key !== 'content')
+        const el = document.createElement(tag)
+        el.className = obj[tag]
+        const children = obj.content.map(this.toHTML)
+        el.append(...children)
+        return el
       }
     }
   }
@@ -164,61 +111,53 @@ export default {
 </script>
 
 <style lang="scss">
-.toolbar-wrapper {
-  position: sticky;
-  top: 22px;
-  height: 0;
-  z-index: 10;
-
-  [id|=toolbar] {
-    position: absolute;
-    top: -45px;
-    width: max-content;
-
-    button[aria-disabled="true"] {
-      display: none;
-    }
-  }
+.tox-pop {
+  max-width: 500px !important;
 }
 
 
 .editor {
   outline: none;
   width: max-content;
+  max-width: 100%;
   min-width: 184px;
-  padding: 4px;
+  padding: 4px 8px;
 
-  &:focus-visible {
+  &::before {
+    cursor: text;
+    margin-left: 10px;
+    color: #757575;
+  }
+
+  &:focus-within {
     >* {
       outline-style: auto;
       outline-color: rgb(0, 95, 204);
-      outline-offset: 5px;
+      outline-offset: .25em;
     }
 
     section {
       position: relative;
-      pointer-events: none;
 
       &::before {
         content: "";
         position: absolute;
-        left: -18px;
-        top: calc(50% - 7px);
+        left: -20px;
+        top: calc(50% - 6px);
         color: grey;
         font-size: 14px;
         line-height: 14px;
-        pointer-events: all;
       }
 
       &.verse::before {
-        content: "→";
+        content: "▶";
         cursor: e-resize;
       }
 
       &.chorus::before {
-        content: "←";
+        content: "◀";
         cursor: w-resize;
-        left: -20px;
+        left: -24px;
       }
     }
   }
@@ -228,7 +167,7 @@ export default {
 
     &::after {
       pointer-events: all;
-      cursor: url('@/assets/cross.png') 8 8, auto;
+      cursor: url('@/assets/icons/cancel.svg') 12 12, auto;
     }
   }
 }
